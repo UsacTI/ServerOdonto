4e9207a2ec1f226d6118ff5c8a2dee8e63b20b22
Javier Hevia <javierhc22@gmail.com>
2021-01-26 15:05:15 -0600

    Update expediente.js

--- app/controllers/expediente.js
+++ app/controllers/expediente.js
@@ -6,19 +6,27 @@ const Expediente = db.Expediente
 exports.createExpediente = (req, res) => {
   const expediente = {}
   try {
-    expediente.evaluacion_clinica = req.body.evaluacion_clinica
-    expediente.roentgenologia = req.body.roentgenologia
-    expediente.precauciones = req.body.precauciones
-    expediente.equipo_diagnostico = req.body.equipo_diagnostico
-    expediente.habitos = req.body.habitos
-    expediente.diagnostico = req.body.diagnostico
-    expediente.oclusion = req.body.oclusion
-    expediente.oclusion_comentario = req.body.oclusion_comentario
+    expediente.mc = req.body.mc
+    expediente.hpe = req.body.hpe
     expediente.hma = req.body.hma
     expediente.hma_comentario = req.body.hma_comentario
     expediente.hoa = req.body.hoa
     expediente.hoa_comentario = req.body.hoa_comentario
     expediente.dolor_dentario = req.body.dolor_dentario
+    expediente.habitos = req.body.habitos
+    expediente.roentgenologia = req.body.roentgenologia
+    expediente.precauciones = req.body.precauciones
+    expediente.evaluacion_clinica = req.body.evaluacion_clinica
+    expediente.oclusion = req.body.oclusion
+    expediente.oclusion_comentario = req.body.oclusion_comentario
+    expediente.roentoenogramas = req.body.roentoenogramas
+    expediente.roentoenogramas_descripcion = req.body.roentoenogramas_descripcion
+    expediente.opciones = req.body.opciones
+    expediente.opciones_descripcion = req.body.opciones_descripcion
+    expediente.consulta = req.body.consulta
+    expediente.estudios_especiales = req.body.estudios_especiales
+    expediente.equipo_diagnostico = req.body.equipo_diagnostico
+    expediente.diagnostico = req.body.diagnostico
     expediente.idpaciente = req.body.idpaciente
     Expediente.create(expediente).then(result => {
       res.status(200).json({
@@ -45,7 +53,7 @@ exports.filterById = (req, res) => {
         paciente: results
       })
     })
-    . catch(error => {
+    .catch(error => {
       // console.log(error)
       res.status(500).json({
         message: 'No se encontró el Expediente con ID =' + id,
