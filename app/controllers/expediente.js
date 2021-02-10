88f76b94e2e5b5b092018952a521611441ea340a
Javier Hevia <javierhc22@gmail.com>
2021-02-10 17:31:41 -0600

    Update expediente.js

--- app/controllers/expediente.js
+++ app/controllers/expediente.js
@@ -191,11 +191,11 @@ exports.InsertarRadiografia = async (req, res) => {
   var radiografia = req.body.radiografia
   console.log(idexpediente)
   console.log(radiografia)
-  const { headers, files } = req
-  const { buffer, originalname: filename } = files[0]
-  const formFile = new FormData()
-  formFile.append('file', buffer, { filename })
-  
+  // const { headers, files } = req
+  // const { buffer, originalname: filename } = files[0]
+  // const formFile = new FormData()
+  // formFile.append('file', buffer, { filename })
+
   // await db.sequelize.query(
   //   `update expedientes
   //   set radiografia= ?
