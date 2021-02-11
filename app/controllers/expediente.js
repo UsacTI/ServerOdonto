65aa1013b6a9c9f4f09262acbb8e7d841d205512
Javier Hevia <javierhc22@gmail.com>
2021-02-11 09:02:33 -0600

    Update expediente.js

--- app/controllers/expediente.js
+++ app/controllers/expediente.js
@@ -199,7 +199,7 @@ exports.InsertarRadiografia = async (req, res) => {
       throw err
     }
     // console.log('Fields', fields)
-    console.log(files.File[0])
+    console.log(files.File)
     // for (const file of Object.entries(files)) {
     //   console.log(file)
     // }
