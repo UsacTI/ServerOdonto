2bd103fdae8588182fb5bec355076640fe20075f
Javier Hevia <javierhc22@gmail.com>
2021-03-16 09:57:14 -0600

    Update paciente.js

--- app/controllers/paciente.js
+++ app/controllers/paciente.js
@@ -218,7 +218,7 @@ exports.updateById = async (req, res) => {
     }
   } catch (error) {
     res.status(500).json({
-      message: 'No se pudo actualizar el paciente con No.idus = ' + req.params.dpi,
+      message: 'No se pudo actualizar el paciente con No.idus = ' + req.body.id,
       error: error.message
     })
   }
