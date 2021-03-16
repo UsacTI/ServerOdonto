3a24e6ed321e3d5bf53fc762c56704ea084ed359
Javier Hevia <javierhc22@gmail.com>
2021-03-16 10:28:58 -0600

    Update paciente.js

--- app/controllers/paciente.js
+++ app/controllers/paciente.js
@@ -192,7 +192,7 @@ exports.updateById = async (req, res) => {
         dpi: req.body.cui,
         direccion: req.body.direccion,
         telefono: req.body.telefono,
-        nohijos: (req.body.nohijos === undefined ? 0 : req.body.nohijos)
+        nohijos: (req.body.nohijos === '' ? 0 : req.body.nohijos)
       //   escolaridad: (req.body.escolaridad === undefined ? '' : req.body.escolaridad),
       //   nivel: (req.body.nivel === undefined ? '' : req.body.nivel),
       //   profesion: (req.body.trauoficio === undefined ? '' : req.body.trauoficio),
